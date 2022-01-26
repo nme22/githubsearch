387af5ff031b71936f08851670f44c89bd28cbfa
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-01-26 16:48:22 -0500

    inital commit

---
+++ components/Navbar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Navbar() {
+  return <h2>Github Search App</h2>;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Navbar>
+      <Component {...pageProps} />
+    </Navbar>
+  );
 }
 
-export default MyApp
+export default MyApp;
