5031287ec2de30af68b5eb2abc4b55c5635c7433
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-01-27 03:28:41 -0500

    tracked state of input field

--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,27 @@
 import { Input, Box, Text, Divider, Button } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 export default function SearchGithub() {
+  const [username, setUsername] = useState('');
+
+  function handleUsername(e) {
+    setUsername(e.target.value);
+  }
+
   return (
     <Box>
       <Text fontSize="lg"> Search for Github Repositories!</Text>
       <Divider />
-      <Input placeholder="Enter a github username" />
-      <Button colorScheme="telegram" position="fixed" ml="3px">
+      <Input
+        placeholder="Enter a github username"
+        onChange={handleUsername}
+        value={username}
+      />
+      <Button
+        colorScheme="telegram"
+        position="fixed"
+        ml="3px"
+        onClick={handleUsername}
+      >
         Search
       </Button>
     </Box>
