6632db8d1d2145701165376de2d81546b8edc4aa
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-01-27 20:16:24 -0500

    github link to user working

--- components/Search.js
+++ components/Search.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Input, Box, Text, Divider, Button } from '@chakra-ui/react';
+import {
+  Input,
+  Box,
+  Text,
+  Divider,
+  Button,
+  Avatar,
+  Heading,
+  VStack,
+  Container,
+  List,
+  ListItem,
+  ListIcon,
+  Link,
+} from '@chakra-ui/react';
+import { LinkIcon, ViewIcon } from '@chakra-ui/icons';
+
 import { useState } from 'react';
 
 export default function SearchGithub() {
   const [username, setUsername] = useState('');
-  const [data, setData] = useState('');
+  const [data, setData] = useState({});
 
   async function handleGithubSearch() {
-    fetch('/api/github', {
+    /// Clean this up a little later there guy
+    await fetch('/api/github', {
       method: 'POST',
       body: JSON.stringify(username),
     })
       .then((response) => response.json())
       .then((data) => setData(data));
-    console.log(data);
   }
 
   function handleUsername(e) {
+    e.preventDefault();
     setUsername(e.target.value);
   }
 
@@ -28,6 +45,7 @@ export default function SearchGithub() {
         placeholder="Enter a github username"
         onChange={handleUsername}
         value={username}
+        position="relative"
       />
       <Button
         colorScheme="telegram"
@@ -35,10 +53,41 @@ export default function SearchGithub() {
         ml="3px"
         onClick={handleGithubSearch}
       >
-        Search
+        Submit
       </Button>
 
-      <h1> {data.login} </h1>
+      {data.data ? (
+        <VStack
+          p={2}
+          m={2}
+          size="md"
+          bgColor="blue.400"
+          borderRadius="lg"
+          borderWidth="thick"
+          borderColor="gray.400"
+        >
+          <Avatar
+            src={data.data.avatar_url}
+            alignSelf="center"
+            size="2xl"
+            borderWidth="thin"
+          />
+          <Heading fontSize="lg" fontFamily="fantasy">
+            {data.data.name}
+          </Heading>
+          <List bgColor="whiteAlpha.400">
+            <ListItem>
+              <ListIcon as={LinkIcon} />
+              <Link href={data.data.html_url}> {data.data.html_url}</Link>
+            </ListItem>
+          </List>
+          <Container>
+            <Text fontWeight="bold" fontFamily="fantasy">
+              {data.data.bio}
+            </Text>
+          </Container>
+        </VStack>
+      ) : null}
     </Box>
   );
 }
