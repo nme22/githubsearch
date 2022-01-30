bdea83aaa14a28381db7b3105596d51c6202f847
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-01-30 00:34:40 -0500

    should build correctly this time

--- components/Search.js
+++ components/Search.js
@@ -29,7 +29,7 @@ export default function SearchGithub() {
   };
 
   async function handleGithubSearch() {
-    fetch('/api/github', {
+    fetch('/api/Github', {
       method: 'POST',
       body: JSON.stringify({
         username,
