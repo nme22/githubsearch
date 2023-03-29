474c075b03d20d7f9791f907108c60315bda18cf
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2023-03-29 14:24:25 -0400

    updated meta contnet

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Github Search</title>
-        <meta
-          name="description"
-          content="A app that lets you search github repos"
-        />
-      </Head>
-    </div>
-  );
+   return (
+      <div>
+         <Head>
+            <title>Github Search</title>
+            <meta
+               name="description"
+               content="A app that lets you search github users"
+            />
+         </Head>
+      </div>
+   );
 }
